bef2f4c3d228c7668c3d0816fa436ebadf100022
ebarrientos1979 <edwin.barrientos@iteraprocess.com>
2024-08-02 20:47:23 -0500

    clase2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mi-primer-app';
+  title = ' Operaciones entres dos variables ';
+  primerNumero: number = 0;
+  segundoNumero: number = 0;
+
+  clickBoton() {
+    console.log(this.primerNumero);
+    console.log(this.segundoNumero);
+  }
 }
